bb150f2011c20c6600b529e2941b58c7b6201cec
Quinnimy <qlamkin@mail.bradley.edu>
2022-01-30 23:08:49 -0600

    Color change walls added to Prototype 1

---
+++ Prototype1/Assets/Scripts/ChangeColorOnCollision.cs
@@ -0,0 +1,22 @@
+﻿/*
+* Quinn Lamkin
+* Assignment 2 Prototype 1
+* color change collision, changes color to red
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//attach to an object, wall here
+public class ChangeColorOnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        //When colliding with player
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //turns object red
+            gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+        }
+    }
+}
