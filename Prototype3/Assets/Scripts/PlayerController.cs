9374c697f7a8145b6e2048c8f787325a365a4f66
Quinnimy <qlamkin@mail.bradley.edu>
2022-02-20 22:21:42 -0600

    Prototype3 Completed

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -18,11 +18,32 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
 
+    //part 2
+    public Animator playerAnimator;
+
+    public ParticleSystem explosionParticle;
+
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+
+    private AudioSource playerAudio;
+
     // Start is called before the first frame update
     void Start()
     {
         //set reference variables to components
         rb = GetComponent<Rigidbody>();
+
+        //pt 2 
+        playerAnimator = GetComponent<Animator>();
+
+        //set reference to Audio Source
+        playerAudio = GetComponent<AudioSource>();
+
+        //start runnin
+        playerAnimator.SetFloat("Speed_f", 1.0f);
 
         //video
         jumpforceMode = ForceMode.Impulse;
@@ -43,20 +64,50 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpForce, jumpforceMode);
             isOnGround = false;
+
+            //part 2 
+            //set trigger to play jump animation
+            playerAnimator.SetTrigger("Jump_trig");
+
+            //stop playing dirt particle on jump
+            dirtParticle.Stop();
+
+            //play jump sound effect
+            playerAudio.PlayOneShot(jumpSound);
         }
     }
+
+    //part 2
+
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            //play dirt particle when player hits ground
+            dirtParticle.Play();
         }
 
         else if( collision.gameObject.CompareTag("Obstacle") && !gameOver)
         {
             Debug.Log("Game Over!");
             gameOver = true;
+
+            //part 2
+
+            //bothered me dirt was kicked up on death still
+            dirtParticle.Stop();
+
+            //play death animation
+            playerAnimator.SetBool("Death_b", true);
+            playerAnimator.SetInteger("DeathType_1", 1);
+
+            //play explosion particle
+            explosionParticle.Play();
+
+            //play collision sound
+            playerAudio.PlayOneShot(crashSound);
         }
     }
 }
