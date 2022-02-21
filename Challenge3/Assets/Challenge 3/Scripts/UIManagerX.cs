7993a16ace3d4c1794e8bfe4cc54a67ab48659bc
Quinnimy <qlamkin@mail.bradley.edu>
2022-02-20 23:30:56 -0600

    Challenge 3 UI script added
    added ui script and text box/canvas

---
+++ Challenge3/Assets/Challenge 3/Scripts/UIManagerX.cs
@@ -0,0 +1,31 @@
+﻿/*
+* Quinn Lamkin
+* Assignment 4 Chellange 3
+* Handles showing the score and win/loss messages
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerX : MonoBehaviour
+{
+
+    public int score = 0;
+    public Text scoreText;
+
+    public PlayerControllerX playerControllerScript;
+
+    public bool won = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
