2530cda5b2f610f6f2435faf887bc80fbc389d72
Quinnimy <qlamkin@mail.bradley.edu>
2022-04-10 17:55:44 -0500

    Prototype4 Video Done

---
+++ Protoype4/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    private Rigidbody enemyRb;
+    GameObject player;
+    public float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        //add force toward the direction from the player to the enemy
+
+        //vector for direction from enemy to player
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        //add force towards player
+        enemyRb.AddForce(lookDirection * speed);
+
+        if (transform.position.y<-10)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Protoype4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
+
+    private float spawnRange = 9;
+
+
+    public int enemyCount;
+    public int waveNumber =1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnEnemyWave(waveNumber);
+        spawnPowerUp(1);
+
+    }
+
+    private void spawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            //instantiate enemy in random postition
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+
+    private void spawnPowerUp(int powerUpsToSpawn)
+    {
+        for (int i = 0; i < powerUpsToSpawn; i++)
+        {
+            //instantiate powerups in random postition
+            Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        }
+    }
+
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+        if (enemyCount ==0)
+        {
+            waveNumber++;
+            spawnEnemyWave(waveNumber);
+            spawnPowerUp(1);
+        }
+    }
+}
